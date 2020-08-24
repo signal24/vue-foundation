6f65f38bd299715b3b16e51252a4105371699648
Sean Ferguson <sean@sgnl24.com>
2020-08-24 08:32:17 -0400

    more array helpers

--- src/helpers/array.js
+++ src/helpers/array.js
@@ -1,3 +1,72 @@
+Object.defineProperty(Array.prototype, 'diff', {
+    enumerable: false,
+    value: function(...args) {
+        return this.filter(val => {
+            for (let i = 0; i < args.length; i++) {
+                if (args[i].includes(val)) {
+                    return false;
+                }
+            }
+            
+            return true;
+        });
+    }
+});
+
+Object.defineProperty(Array.prototype, 'first', {
+    enumerable: false,
+    value: function() {
+        return this[0] || undefined;
+    }
+});
+
+Object.defineProperty(Array.prototype, 'intersect', {
+    enumerable: false,
+    value: function(...args) {
+        return this.filter(val => {
+            for (let i = 0; i < args.length; i++) {
+                if (args[i].includes(val)) {
+                    return true;
+                }
+            }
+            
+            return false;
+        });
+    }
+});
+
+Object.defineProperty(Array.prototype, 'keyBy', {
+    enumerable: false,
+    value: function(keyProp) {
+        let result = {};
+        this.forEach(elem => {
+            result[elem[keyProp]] = elem;
+        });
+        return result;
+    }
+});
+
+Object.defineProperty(Array.prototype, 'last', {
+    enumerable: false,
+    value: function() {
+        return this.length ? this[this.length - 1] : undefined;
+    }
+});
+
+Object.defineProperty(Array.prototype, 'pluck', {
+    enumerable: false,
+    value: function(prop, keyProp) {
+        if (typeof keyProp === 'undefined')
+            return this.map(elem => elem[prop]);
+
+        let result = {};
+        this.forEach(elem => {
+            result[elem[keyProp]] = elem[prop];
+        });
+        return result;
+    }
+});
+
 Object.defineProperty(Array.prototype, 'remove', {
     enumerable: false,
     value: function(element) {
@@ -11,20 +80,6 @@ Object.defineProperty(Array.prototype, 'replace', {
     value: function(element, replacement) {
         let index = this.indexOf(element);
         index > -1 && this.splice(index, 1, replacement);
-    }
-});
-
-Object.defineProperty(Array.prototype, 'first', {
-    enumerable: false,
-    value: function() {
-        return this[0] || undefined;
-    }
-});
-
-Object.defineProperty(Array.prototype, 'last', {
-    enumerable: false,
-    value: function() {
-        return this.length ? this[this.length - 1] : undefined;
     }
 });
 
