dd6993e899c7d67748f82349c6712a5059a66581
Sean Ferguson <sean@sgnl24.com>
2024-02-11 00:15:14 -0500

    confirm button options

--- src/directives/confirm-button.ts
+++ src/directives/confirm-button.ts
@@ -1,6 +1,12 @@
+import type { DirectiveBinding } from 'vue';
 import type { ObjectDirective } from 'vue';
 
-export const vConfirmButton: ObjectDirective<HTMLElement, void> = {
+interface ConfirmButtonOptions {
+    text?: string | null;
+    class?: string;
+}
+
+export const vConfirmButton: ObjectDirective<HTMLElement, ConfirmButtonOptions> = {
     mounted: fn
 };
 
@@ -14,9 +20,10 @@ interface IConfirmElement {
     [ConfirmState]?: IConfirmState;
 }
 
-function fn(el: HTMLElement & IConfirmElement) {
+function fn(el: HTMLElement & IConfirmElement, binding: DirectiveBinding<ConfirmButtonOptions>) {
     el.addEventListener('click', e => {
         const now = Date.now();
+        const confirmText = binding.value?.text !== undefined ? binding.value.text : 'Confirm';
 
         if (el[ConfirmState]) {
             if (now - el[ConfirmState].initTime < 300) {
@@ -35,15 +42,18 @@ function fn(el: HTMLElement & IConfirmElement) {
             initTime: now,
             preconfirmHtml: el.innerHTML,
             resetHandler: () => {
-                el.innerHTML = confirmState.preconfirmHtml;
+                if (confirmText) el.innerHTML = confirmState.preconfirmHtml;
+                if (binding.value?.class) el.classList.remove(binding.value.class);
                 el.blur();
 
                 el.removeEventListener('mouseout', confirmState.resetHandler);
                 delete el[ConfirmState];
             }
         };
+
         el[ConfirmState] = confirmState;
-        el.innerHTML = 'Confirm';
+        if (confirmText) el.innerHTML = confirmText;
+        if (binding.value?.class) el.classList.add(binding.value.class);
 
         el.addEventListener('mouseout', confirmState.resetHandler);
     });
