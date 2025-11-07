3c57c10d86fb92d9dd9fda263d7246845239c255
Sean Ferguson <sean@sgnl24.com>
2025-11-07 06:02:25 -0500

    fix sticky-min-width

--- src/directives/sticky-min-width.ts
+++ src/directives/sticky-min-width.ts
@@ -5,8 +5,10 @@ const ObserverMap = new WeakMap<HTMLElement, ResizeObserver>();
 export const vStickyMinWidth: ObjectDirective<HTMLElement> = {
     beforeMount(el) {
         const observer = new ResizeObserver(() => {
-            const minWidth = el.clientWidth;
-            el.style.minWidth = `${minWidth}px`;
+            const computedMinWidthStr = window.getComputedStyle(el).minWidth.match(/^(\d+)px$/)?.[1];
+            const computedMinWidth = computedMinWidthStr ? parseInt(computedMinWidthStr, 10) : 0;
+            if (el.clientWidth <= computedMinWidth) return;
+            el.style.minWidth = `${el.clientWidth}px`;
         });
         ObserverMap.set(el, observer);
         observer.observe(el);
